1878c4dfad56b6b94e34c613bf770f584ec78290
BerimboloEnterprises <marcus.t.lindstedt@gmail.com>
2016-10-07 11:14:43 +0200

    Created a structure for organizing files. Removed Player prefab. Created prefabs for Simone and Phillippa (Phillippa is exactly the same as Player was right now). Renamed some objects.

---
+++ S41/Assets/Scripts/Character Scripts/Simone/Simone_Attack.cs
@@ -0,0 +1,65 @@
+﻿#region Using Statements
+
+using UnityEngine;
+using System.Collections;
+
+#endregion
+
+public class Simone_Attack : MonoBehaviour
+{
+    #region Variables
+
+    public Transform m_transformOrigin; // Where the attack will spawn on the character
+    public Rigidbody m_bullet;
+
+    private float m_bulletLaunchForce;
+
+    #endregion
+
+    #region On Enable
+
+    /// <summary>
+    /// Sets the force which will propel the bullet
+    /// </summary>
+    private void OnEnable()
+    {
+        m_bulletLaunchForce = 30f;
+    }
+
+    #endregion
+
+    #region Start
+
+    void Start()
+    {
+
+    }
+
+    #endregion
+
+    #region Update
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            S_autoAttack();
+        }
+    }
+
+    #endregion
+
+    #region Simone Auto Attack
+    /// <summary>
+    /// Simones auto attack.
+    /// Creates a bullet and shoots in the direction that Simone is facing.
+    /// </summary>
+    private void S_autoAttack()
+    {
+        Rigidbody bulletInstance = Instantiate(m_bullet, m_transformOrigin.position, m_transformOrigin.rotation) as Rigidbody;
+        bulletInstance.velocity = m_bulletLaunchForce * m_transformOrigin.forward;
+    }
+
+    #endregion
+
+}
