2fe79436496039d9575bf034ee0068ab1d85167d
Morozbea <morozbea@freemail.hu>
2016-10-15 17:15:00 +0200

    works ! :P
    build and run-> main menu works, in game you all die -> game over
    restart eller quit

---
+++ S41/Assets/Scripts/Menu Scripts/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+
+    public GameObject quitGameButton;
+    public GameObject startGameButton;
+	// Use this for initialization
+	void Start () {
+        startGameButton = GameObject.Find("RestartButton");
+        quitGameButton = GameObject.Find("Quit");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Restart();
+        Quit();
+	}
+
+
+    public void Restart()
+    {
+        if (Input.GetKeyDown("joystick button 2") || Input.GetKeyDown(KeyCode.F1))
+        {
+            SceneManager.LoadScene("MainMenu");
+            Debug.Log("Restart");
+        }
+    }
+
+    public void Quit()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace) || Input.GetKeyDown("joystick button 6"))
+        {
+            Application.Quit();
+        }
+    }
+}
