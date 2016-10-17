2ccfcd8c10b1bee6b619c6545da7723217c192d8
Bernhard <benny_boy_sos@hotmail.com>
2016-10-17 17:40:51 +0200

    Fixade så MeshColliders är mindre än TriggerColliders så Simones skott inte knuffar iväg andra karaktärer, gav Phillippa en melee-attack

---
+++ S41/Assets/Scripts/WhiskAttack.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhiskAttack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            if (other.gameObject.tag == "Enemy")
+            {
+                other.gameObject.GetComponent<EnemyHealth>().Hit(10);       //Hit(10) has to be changed to Hit(dmg variable in simone script)
+                //Destroy(gameObject);
+            } 
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
