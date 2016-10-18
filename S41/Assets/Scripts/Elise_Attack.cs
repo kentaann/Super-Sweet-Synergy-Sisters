30986ee14efb1608a8628ad52acb4bffe9eebbcf
Bernhard <benny_boy_sos@hotmail.com>
2016-10-18 12:10:22 +0200

    Added Elise trap prefabs

---
+++ S41/Assets/Scripts/Elise_Attack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elise_Attack : MonoBehaviour 
+{
+
+    public Transform m_transformOrigin;
+    public Collider m_trap;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.O) || Input.GetButton("X360_B"))
+        {
+            E_LayTrap();
+        } 
+	}
+
+    private void E_LayTrap()
+    {
+        Collider m_trapInstance = Instantiate(m_trap, m_transformOrigin.position, m_transformOrigin.rotation) as Collider;
+    }
+}
