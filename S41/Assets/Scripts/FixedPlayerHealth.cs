dedcc93d9d91b388ed31b9b2de0446ae8bc071b4
kentaann <kennethstrom@hotmail.com>
2016-10-19 12:39:44 +0200

    Health class

---
+++ S41/Assets/Scripts/FixedPlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedPlayerHealth : MonoBehaviour {
+
+    public int health;
+    public int lookForDamage;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //health = 100;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookForDamage = health;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+}
