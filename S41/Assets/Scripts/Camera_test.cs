d7508f3ab230665ef842213926b73daf04455aac
JonathanPisch <jonte_pisch@hotmail.com>
2016-10-26 15:38:12 +0200

    camera improvement

--- S41/Assets/Scripts/Camera_test.cs
+++ S41/Assets/Scripts/Camera_test.cs
@@ -3,31 +3,51 @@ using System.Collections;
 
 public class Camera_test : MonoBehaviour
 {
-    //public GameObject player;
+    
     private Vector3 offset;
     public Transform[] m_Targets;
     private Vector3 camera_startPos;
-    // Use this for initialization
+   
     public float Speed;
+    public Vector3 camPos;
+    public float changeYpos = 0;
+    public int oldPos;
+    public float oneDecimal = 0;
+    public float oldPosOneDecimal = 0;
+    public Vector3 averagePosition;
+    public Vector3 camActualPosition;
+
+    public GameObject whoIsTheObject;
+    public float newdistance;
+
+
+    void Start()
+    {
+        //camera_startPos = new Vector3(3, 13.5f, -8.5f);
+
+        //offset = FindAveragePosition() + camera_startPos;
+        averagePosition = FindAveragePosition();
+        camera_startPos = new Vector3(3, 17f, -8.5f);
+        offset = FindAveragePosition() + camera_startPos;
+        oldPosOneDecimal = (float)System.Math.Round(newdistance, 1);
+    }
+
 
     void Update()
     {
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position , new Vector3(0, 20, -10), Time.deltaTime * Speed);
+        //Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position , new Vector3(0, 20, -10), Time.deltaTime * Speed);
+        camPos = Camera.main.transform.position;
+        ZoomByItSelf();
+        ZoomWithButtons();
     }
 
-
-
-void Start()
-    {
-        camera_startPos = new Vector3(3, 13.5f, -8.5f);
-
-        offset = FindAveragePosition() + camera_startPos;
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = FindAveragePosition() + offset;
+        //transform.position = FindAveragePosition() + offset;
+        //Nytt
+        transform.position = new Vector3(FindAveragePosition().x + offset.x, FindAveragePosition().y + offset.y + changeYpos, FindAveragePosition().z + offset.z);
+        //Se kamerans postion utan offset
+        //transform.position = new Vector3(FindAveragePosition().x, FindAveragePosition().y + offset.y + changeYpos, FindAveragePosition().z);
 
     }
     private Vector3 FindAveragePosition()
@@ -47,6 +67,54 @@ void Start()
         return averagePos;
     }
 
+    public void ZoomByItSelf()
+    {
+        Transform cameraTarget = GetFarthestPlayer(m_Targets);
+        whoIsTheObject = cameraTarget.gameObject;
+        if (cameraTarget)
+        {
+            newdistance = Vector3.Distance(FindAveragePosition(), cameraTarget.position);
+            oneDecimal = (float)System.Math.Round(newdistance, 1);
+            if (oneDecimal != oldPosOneDecimal && oneDecimal > oldPosOneDecimal)
+            {
+                changeYpos += 0.2f;
+                oldPosOneDecimal = oneDecimal;
+            }
+            if (oneDecimal != oldPosOneDecimal && oneDecimal < oldPosOneDecimal)
+            {
+                changeYpos -= 0.2f;
+                oldPosOneDecimal = oneDecimal;
+            }
+        }
+    }
+    private void ZoomWithButtons()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            changeYpos++;
+        }
+        else if (Input.GetKey(KeyCode.V))
+        {
+            changeYpos--;
+        }
+    }
+
+    Transform GetFarthestPlayer(Transform[] player)
+    {
+        Transform largest = null;
+        float maxDist = 5;
+        Vector3 currentPos = FindAveragePosition();
+        foreach (Transform t in m_Targets)
+        {
+            float dist = Vector3.Distance(t.position, currentPos);
+            if (dist > maxDist)
+            {
+                largest = t;
+                maxDist = dist;
+            }
+        }
+        return largest;
+    }
 
 }
 
