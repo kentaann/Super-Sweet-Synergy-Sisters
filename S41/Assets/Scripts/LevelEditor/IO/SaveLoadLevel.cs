766eeed5dd2599bf3fdec58f9032a20761191f17
Morozbea <morozbea@freemail.hu>
2016-10-28 23:25:24 +0200

    saveloadlevelscript
    not done cause I have to read about the serialization to save all level
    data in a binary format.

---
+++ S41/Assets/Scripts/LevelEditor/IO/SaveLoadLevel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using Entities;
+// using serialization to be able to save in file read more about to how
+
+public class SaveLoadLevel : MonoBehaviour {
+
+	public void SaveMap(string fileName, Level levelData){
+        //var levelDataBytes = Serialization.UnitySerializer.Serialize(levelData);
+        if(!Directory.Exists (Application.persistentDataPath + "/CustomLevels/"))
+            Directory.CreateDirectory(Application.persistentDataPath + "/CustomLevels/");
+
+        var filePath = Application.persistentDataPath + "/CustomLevels/" + fileName + ".lvl";
+
+        using (var fs = new FileStream(filePath,FileMode.Create,FileAccess.Write)){
+            using (var bw = new BinaryWriter(fs)){
+             //   bw.Write(levelDataBytes);
+                bw.Flush ();
+            }
+        }
+
+    }
+
+    public Level LoadMap(string fileName){
+        Level loadedLevel = null;
+
+        var filePath = Application.persistentDataPath + "/CustomLevels/" + fileName + ".lvl";
+
+        using (var fs = new FileStream(filePath,FileMode.Open,FileAccess.Read)){
+            //loadedLevel = UnitySerializer.Deserialize<Level>(fs);
+        }
+
+        Debug.Log (loadedLevel.LevelName);
+        Debug.Log (loadedLevel.LevelDescription);
+        Debug.Log (loadedLevel.Author);
+        //Debug.Log (loadedLevel.LevelParts.Count);
+
+        return loadedLevel;
+    }
+}
+
