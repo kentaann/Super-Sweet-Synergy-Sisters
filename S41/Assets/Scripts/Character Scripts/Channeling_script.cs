40f0c37be91aff0a6700781835159bcfdf10a2fa
Marcus Lindstedt <marcus.t.lindstedt@gmail.com>
2016-10-30 19:36:16 +0100

    Added prefab for channeling placeholder and a channeling script.

---
+++ S41/Assets/Scripts/Character Scripts/Channeling_script.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Channeling_script : MonoBehaviour 
+{
+    public float m_maxRange;                 // The max range of the ability
+    public float m_growthRate;               // The rate at which the beam grows
+    public float m_waitTime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(Expand());
+	}
+
+    IEnumerator Expand()
+    {
+        float timer = 0;
+
+        while (true)
+        {
+            while (m_maxRange > transform.localScale.x)
+            {
+                timer += Time.deltaTime;
+                transform.localScale += new Vector3(0, 1, 0) * Time.deltaTime * m_growthRate;
+                transform.localPosition += new Vector3(2, 0, 0) * Time.deltaTime;
+                yield return null;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
