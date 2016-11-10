e37c4c34d134467c9237a0d3ba5183d39f2413a2
Morozbea <morozbea@freemail.hu>
2016-11-10 23:21:01 +0100

    trying another solution for Level editor

---
+++ S41/Assets/Scripts/LevelEditor/NewTryLE/LevelBuildInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+public class LevelBuildInfo : MonoBehaviour {
+
+    public List<ObjectInfos> objList;
+    public LevelBuildInfo() { }
+
+    public LevelBuildInfo(GameObject rootObject)
+    {
+        objList = new List<ObjectInfos>();
+        //foreach(var child in rootObject)
+        //{
+        //    objList.Add(new ObjectInfos(child));
+        //}
+    }
+	     
+}
---
+++ S41/Assets/Scripts/LevelEditor/NewTryLE/ObjectInfos.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectInfos : MonoBehaviour {
+
+	//stores info om objecter
+    public string solveigPrefab;
+    public Vector3 solveigPos;
+    public Quaternion solveigRot;
+
+    public ObjectInfos()
+    {
+
+    }
+
+    public ObjectInfos(Transform obj)
+    {
+        solveigPrefab = obj.name.Replace(" (Clone)", string.Empty);
+        solveigPos = obj.position;
+        solveigRot = obj.rotation;
+    }
+
+}
