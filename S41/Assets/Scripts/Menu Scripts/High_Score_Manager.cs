6553e72a9b9d4a89e42864f57f1e32caa4592f60
Morozbea <morozbea@freemail.hu>
2016-11-26 17:09:17 +0100

    highscore manager script

--- S41/Assets/Scripts/Menu Scripts/High_Score_Manager.cs
+++ S41/Assets/Scripts/Menu Scripts/High_Score_Manager.cs
@@ -12,7 +12,7 @@ public class High_Score_Manager : MonoBehaviour
     {
         get
         {
-            if(m_Instance == null)
+            if (m_Instance == null)
             {
                 m_Instance = new GameObject("High_Score_Manager").AddComponent<High_Score_Manager>();
             }
@@ -22,7 +22,7 @@ public class High_Score_Manager : MonoBehaviour
 
     void Awake()
     {
-        if(m_Instance == null)
+        if (m_Instance == null)
         {
             m_Instance = this;
         }
@@ -33,13 +33,95 @@ public class High_Score_Manager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Saving the high score
+    public void SaveHighScore(string players, int score)
+    {
+        List<Scoring> highScore_List = new List<Scoring>();
+
+        int i = 1;
+
+        while (i <= ScoreBoardLength && PlayerPrefs.HasKey("HighScore" + i + "score"))
+        {
+            Scoring temp = new Scoring();
+            temp.score = PlayerPrefs.GetInt("HighScore" + i);
+            highScore_List.Add(temp);
+            i++;
+        }
+        if (highScore_List.Count == 0)
+        {
+            Scoring _temp = new Scoring();
+            _temp.score = score;
+            highScore_List.Add(_temp);
+
+        }
+        else
+        {
+            for (i = 1; i <= highScore_List.Count && i <= ScoreBoardLength; i++)
+            {
+                if(score > highScore_List[i-1].score)
+                {
+                    Scoring _temp = new Scoring();
+                    _temp.score = score;
+                    highScore_List.Insert(i - 1, _temp);
+                    break;
+                }
+                if(i == highScore_List.Count && i < ScoreBoardLength)
+                {
+                    Scoring _temp = new Scoring();
+                    _temp.score = score;
+                    highScore_List.Add(_temp);
+                    break;
+                }
+            }
+        }
+        i = 1;
+        while(i <= highScore_List.Count && i <= ScoreBoardLength)
+        {
+            PlayerPrefs.SetInt("HighScore" + i + "score", highScore_List[i - 1].score);
+            i++;
+        }
+    }
+
+    // a list of the existing scores
+    public List<Scoring> GetHighScores()
+    {
+        List<Scoring> highScores = new List<Scoring>();
+        int i = 1;
+        while(i <= ScoreBoardLength && PlayerPrefs.HasKey("HighScore" + i + "score"))
+        {
+            Scoring temp = new Scoring();
+            temp.score = PlayerPrefs.GetInt("HighScore" + i + "score");
+            highScores.Add(temp);
+            i++;
+        }
+        return highScores;
+    }
+
+    // Clears the scoreboard - for testing
+    public void ClearScoreBoard()
+    {
+        List<Scoring> highScores = GetHighScores();
+        for (int i = 1; i < highScores.Count; i++)
+        {
+            PlayerPrefs.DeleteKey("HighScore" + i + "score");
+        }
+    }
+
+    // dont know if it needs or not.
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.Save();
+    }
 }
