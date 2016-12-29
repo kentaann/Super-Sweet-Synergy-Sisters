7b4fad67f7a5f7efbf55490a9516f7ef56975254
kentaann <kennethstrom@hotmail.com>
2016-12-29 05:24:26 +0100

    fixade nu så att melee enemies skadar hela tiden, bytt till bounds ist för onCollision, pillade oxå i playerHealth där vissa värden var fel som var kopplade till health baren till players, nu ska det se bra ut

--- S41/Assets/Scripts/Character Scripts/FixedPlayerHealth.cs
+++ S41/Assets/Scripts/Character Scripts/FixedPlayerHealth.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class FixedPlayerHealth : MonoBehaviour {
 
     public int currentHealth;
-    private int maxHealth;
+    public int maxHealth;
     public int lookForDamage;
-
+    public float dmgTime = 0.1f;
 
     private float m_timer;
     private const float m_GODTIMER = 5.0f;
@@ -26,11 +26,33 @@ public class FixedPlayerHealth : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+
+
+        foreach (var e in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            dmgTime += Time.deltaTime;
+
+            if (gameObject.GetComponent<Collider>().bounds.Intersects(e.GetComponent<Collider>().bounds))
+            {
+                if (dmgTime > 0.3f)
+                {
+                    TakeDamage(20);
+                    dmgTime = 0;
+                    Debug.Log("Hit enemy");
+                }
+            }
+        }
+        
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
     }
 
     public void GetHeal(float heal)
@@ -53,9 +75,8 @@ public class FixedPlayerHealth : MonoBehaviour {
 
         if (other.gameObject.tag == "Enemy")
         {
-            TakeDamage(20);         //here is where damage is taken
+            //TakeDamage(20);         //here is where damage is taken
             enemyBody.AddExplosionForce(1000f, transform.position, 5f);         //players are supposed to be pushed away when damaged
-            Debug.Log("Hit enemy");
         }
     }
 }
